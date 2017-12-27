5024c9a94a45eda029ce7d24c4482fa23d63a113
ntrelat <ntrelat@actweb.fr>
2017-12-27 14:20:36 +0100

    retour

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -6,42 +6,10 @@ global.Popper = require('popper.js');
 var Slick = require('slick-carousel');
 require('./case-study');
 require('./slider-home');
-//require('./aw_navigation');
-//require('./jquery.scrollfx');
 
 var bootstrap = require('bootstrap');
 
-var Twitter = require('twitter');
-
-var client = new Twitter(
-  'v49GjvFaG0aS20YySUodb4AN0',
-  'bNBLSpgnuxyh7DPF43cCquSQb8yW0mzSDfzeg4dCroipXnQ0C7',
-  '33459676-0ftc10sFDVPuIpegegD3ynEjHvCvY9aQd1zemAZ9P',
-  'pFhV5A5fbMuPnFZ8liIMerMuhiYadBZTbqlX7nvLCLdRK'
-);
-var params = {screen_name: 'FranckOhrel'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
-//client.get('search/tweets', {q: 'digital'}, function(error, tweets, response) {
-//  console.log(error);
-//});
-
 jQuery(document).ready(function ($) {
-
-
-
-//  var translateUp = $('.js-wrapper-tranlateUp');
-//  var translateDown = $('.js-wrapper-tranlateDown');
-//  if (translateDown.length >= 1) {
-//    translateDown.scrollfx({translateValue: 30});
-//  }
-//  if (translateUp.length >= 1) {
-//    translateUp.scrollfx({'negative': true, translateValue: 30});
-//  }
-
   $('.nav-link').on('click', function (e) {
     e.preventDefault();
 
